b396529356c692d03657bf9c652f802127c65579
Adam Georgiou <me@adamgeorgiou.com>
2024-05-17 19:01:25 -0400

    Hot fix and test print

--- auth/token_verifier.go
+++ auth/token_verifier.go
@@ -490,5 +490,10 @@ func findMaxAge(resp *http.Response) (*time.Duration, error) {
 			return &duration, nil
 		}
 	}
-	return nil, errors.New("Could not find expiry time from HTTP headers")
+
+	fmt.Println("Adam was here.")
+
+	// fix "cache-control: private" issue
+	oneHour := 1 * time.Hour
+	return &oneHour, nil
 }
